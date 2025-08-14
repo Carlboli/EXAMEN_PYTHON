676f64ad555bdfcd0f9685e58326b744a5a57daf
Carlboli <PABLOBERRIO14@outlook.es>
2025-08-13 20:43:55 -0500

    Añadido main.py con código inicial del examen

---
+++ src/main.py
@@ -0,0 +1,7 @@
+def main():
+    print("=== Proyecto Python - EXAMEN PRIMER MOMENTO ===")
+    print("Este es el inicio del examen.")
+    print("Aquí cargaremos datos, calcularemos estadísticas y generaremos un reporte.")
+
+if __name__ == "__main__":
+    main()
